089e648ff40ce9b2ab0846561f186ce8853ae352
Ely Prado <elyfprado@gmail.com>
2021-03-23 21:13:26 -0300

    versão 1603

--- Assets/Scripts/ControleJogador.cs
+++ Assets/Scripts/ControleJogador.cs
@@ -1,38 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.UI;
+
+//adiciona o componente CharacterController automaticamente
+[RequireComponent(typeof(CharacterController))]
 
 public class ControleJogador : MonoBehaviour
 {
+    public float speed = 7.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Transform playerCameraParent;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 60.0f;
+    public Text txtPontuacao;
+    private int pontuacao = 0;
 
-    private Rigidbody rb;
-    public float velocidade;
-    private bool pulando = false;
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 rotation = Vector2.zero;
 
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+        rotation.y = transform.eulerAngles.y;
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        float pulo = 0;
-        if (Input.GetKey(KeyCode.Space) && pulando == false)
+        if (characterController.isGrounded)
         {
-            pulo = 50;
-            pulando = true;
+            // Se o jogador estiver no chão, então pode se mover
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            float curSpeedX = speed * Input.GetAxis("Vertical");
+            float curSpeedY = speed * Input.GetAxis("Horizontal");
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
         }
-        Vector3 mov = new Vector3(h, pulo, v);
-        rb.AddForce(mov * velocidade);
+
+        // Aplica gravidade
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move o jogador
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetButton("Fire1")) {
+            rotation.y -= 1 * lookSpeed;
+        } else if (Input.GetButton("Fire2")) { 
+            rotation.y += 1 * lookSpeed;
+        } else {
+            rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+        }
+
+        // Gira a Câmera
+
+        rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+        playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+        transform.eulerAngles = new Vector2(0, rotation.y);
     }
 
-    void OnCollisionEnter(Collision col)
+    void OnTriggerEnter(Collider other)
     {
-        pulando = false;
+        if (other.gameObject.CompareTag("Moeda"))
+        {
+            other.gameObject.SetActive(false);
+            pontuacao++;
+            txtPontuacao.text = "" + pontuacao;
+        }
     }
+
 }
